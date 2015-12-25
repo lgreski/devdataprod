f7dd7de6a029e1a2a44d55250843d339c807f913
Len Greski <len@ageretechnology.com>
2015-12-24 17:09:11 -0800

    Initial repository commit for December 2015 devdataprod class.

---
+++ ui.R
@@ -0,0 +1,30 @@
+#
+# ui.R -- devdataprod project
+#
+shinyUI(pageWithSidebar(
+     headerPanel("mtcars Scatterplot Matrix"),
+     sidebarPanel(
+          checkboxGroupInput("indepVars",
+                             label= h3("Variable List"),
+                             choices= list("mpg" = 1,
+                                           "cyl" = 2,
+                                           "disp" = 3,
+                                           "hp" = 4,
+                                           "drat" = 5,
+                                           "wt" = 6,
+                                           "qsec" = 7,
+                                           "vs" = 8,
+                                           "am" = 9,
+                                           "gear" = 10,
+                                           "carb" = 11)
+                             # ,selected=c(1,2)
+          )
+     ),
+mainPanel(
+
+     textOutput("text1"),
+     br(),
+     br(),
+     plotOutput("plot1")
+)     
+))
